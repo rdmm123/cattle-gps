548f67cb7575a509074af5f5e9c5e51d65ec3c47
rdmm123 <rdmarenco@uninorte.edu.co>
2022-04-12 23:04:34 -0500

    decoder js

---
+++ decoder.js
@@ -0,0 +1,49 @@
+function dec2bin(dec) {
+    var binString = (dec >>> 0).toString(2);
+    for (var i = 0; i < 16-binString.length; i++) {
+        binString = "0" + binString;
+    }
+    return binString;
+  }
+
+function getSignedInteger(bits) {
+    let negative = (bits[0] === '1');
+    if (negative) {
+        let inverse = '';
+        for (let i = 0; i < bits.length; i++) {
+            inverse += (bits[i] === '0' ? '1' : '0');
+        }
+        return (parseInt(inverse, 2) + 1) * -1;
+    } else {
+        return parseInt(bits, 2);
+    }
+}
+
+function decodeUplink(input) {
+    var data = {};
+
+    data.depto = input.bytes[0];
+    data.finca = input.bytes[1]<<8 | input.bytes[2];
+    data.vaca = input.bytes[3]<<8 | input.bytes[4];
+
+    var latRaw = input.bytes[5]<<8 | input.bytes[6];
+    console.log(dec2bin(10));
+    data.lat = getSignedInteger(dec2bin(latRaw))/100;
+    var lngRaw = input.bytes[7]<<8 | input.bytes[8];
+    data.lng = getSignedInteger(dec2bin(lngRaw))/100;
+
+    console.log(data.lat)
+    console.log(data.lng)
+
+    return {
+        data: data,
+        warnings: [],
+        errors: []
+    };
+}
+
+var input = {
+    bytes: [0x21, 0x01, 0xC8, 0x00, 0x7B, 0x04, 0x4C, 0xE3, 0x18]
+}
+
+console.log(decodeUplink(input));
